fd5b3f1526b70d486080c612eebc403dad2b008e
Philippe Saint-Amand <saintaph@gmail.com>
2023-01-13 19:18:46 +0100

    BoilerPlate V 0.0.2

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'P. Saint-Amand'
 __appname__ = 'BoilerPlate'
-# __appname__, _ = os.path.splitext(os.path.basename(__file__))
-__version__ = 'V 0.0.1'
+__version__ = 'V 0.0.2'
 
 # Standard Python Modules
 import logging
@@ -11,33 +10,40 @@ from pathlib import Path
 from typing import Any
 
 # External Python Modules
-# import typer
+# import pandas
 
 # Personal Python Modules
-import utils
-from constants import *
+from params import *
+from utils.coloredlog import get_logger
+from utils.filename import FileName     #CSVFile, ParameterFile
 
-def get_logger(logger_name:str=None, console_loglevel:int=LOGLEVEL_SUCCESS, file_loglevel:int=LOGLEVEL_DISABLE, logfile:Path=None) -> utils.ColorLogger:
-    if not logfile and file_loglevel != LOGLEVEL_DISABLE:
-        logfile = os.path.join(LOG_DIR,__appname__+".log")
-    if not logger_name:
-        logger_name, _ = os.path.splitext(os.path.basename(__file__))
+### Global Variables
+### uncomment lines below if you want a dynamic log name instead
+# LOG_FILE = os.path.join(LOG_DIR,__appname__+".log")
+# logfilename_object = FileName(OUT_FILE)
+# logfilename_object.add_subname("subname")
+# logfilename_object.add_datetime()
+# LOG_FILE = logfilename_object.fullpath
+# LOGLEVEL_FILE = logging.DEBUG
 
-    
-    logging.addLevelName(LOGLEVEL_SUCCESS, 'SUCCESS')
-    log_options = utils.ColorLoggerOptions(logfile_name=logfile, console_logging_level=console_loglevel, logfile_logging_level=file_loglevel)
-    logger = utils.ColorLogger(name=__appname__, options=log_options)
-    # save_logger_options(log_options)
-    return logger
+### uncomment lines below if you want a dynamic name instead
+# OUT_FILE = os.path.join(OUT_DIR,__appname__+".csv")
+# outfilename_object = FileName(OUT_FILE)
+# outfilename_object.add_subname("subname")
+# outfilename_object.add_datetime()
+# OUT_FILE = outfilename_object.fullpath
 
 def init():
     """ Clear Screen, display banner & start the logger. """
     CONSOLE.clear_screen()
-    if BANNER:
-        print(CONSOLE.get_app_banner(selection="random", banner_lst=banner_lst, appversion=__version__, creator="Designed by " + __author__))
+    if BANNER_DISPLAY:
+        print(CONSOLE.get_app_banner(selection="random", banner_lst=BANNERS, appversion=__version__, creator="Designed by " + __author__))
     global logger
-    logger = get_logger(logger_name=__appname__, console_loglevel=LOGLEVEL_CONSOLE, file_loglevel=LOGLEVEL_FILE)
-    logger.debug("Debug Mode Activated")
+    logger = get_logger(logger_name=__appname__, console_loglevel=LOGLEVEL_CONSOLE, file_loglevel=LOGLEVEL_FILE, logfile=LOG_FILE)
+    logger.info(f"Application Start")
+    logger.info(f"Logging levels : Console={LOGLEVEL_CONSOLE}; File={LOGLEVEL_FILE}; Logfile='{LOG_FILE}'")
+    logger.debug("Confirm Debug Mode is Activated")
 
 if __name__ == "__main__":
     init()
+    #TODO: insert your code here (ideally by calling functions)
--- main_cli.py
+++ main_cli.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'P. Saint-Amand'
-__appname__ = 'BoilerPlate'
-# __appname__, _ = os.path.splitext(os.path.basename(__file__))
-__version__ = 'V 0.0.1'
+__appname__ = 'BoilerPlate_cli'
+__version__ = 'V 0.0.2'
 
 # Standard Python Modules
 import logging
@@ -14,12 +13,19 @@ from typing import Any
 import typer
 
 # Personal Python Modules
-import utils
-from constants import *
+from params import *
+from utils.coloredlog import get_logger
+from utils.filename import FileName     #CSVFile, ParameterFile
 
 ### Global Variables
+# Possible values for a log level using logging module: CRITICAL:50; ERROR:40; WARNING:30; INFO:20, DEBUG:10
+# Possible values for a log level using CONSTANTS (can be adapted in init.py): LOGLEVEL_SUCCESS:15; LOGLEVEL_DISABLE:99999
+LOGLEVEL_CONSOLE = LOGLEVEL_SUCCESS
+LOGLEVEL_FILE = LOGLEVEL_DISABLE
+
 all_args={}
 output_format="txt"
+
 
 def callback_outdir(value:Path) -> Path:
     if value and not value.is_dir() and os.path.splitext(value)[1]:
@@ -29,27 +35,14 @@ def callback_outdir(value:Path) -> Path:
 def callback_version(value:bool) -> None:
     if value:
         # print(f"{__appname__} {__version__}")
-        print(CONSOLE.get_app_banner(selection="random", banner_lst=banner_lst, appversion=__version__, creator="Designed by " + __author__))
+        print(CONSOLE.get_app_banner(selection="random", banner_lst=BANNERS, appversion=__version__, creator="Designed by " + __author__))
         raise typer.Exit()
 
-def get_logger(logger_name:str=None, console_loglevel:int=LOGLEVEL_SUCCESS, file_loglevel:int=LOGLEVEL_DISABLE, logfile:Path=None) -> utils.ColorLogger:
-    if not logfile and file_loglevel != LOGLEVEL_DISABLE:
-        logfile = os.path.join(LOG_DIR,__appname__+".log")
-    if not logger_name:
-        logger_name, _ = os.path.splitext(os.path.basename(__file__))
-
-    
-    logging.addLevelName(LOGLEVEL_SUCCESS, 'SUCCESS')
-    log_options = utils.ColorLoggerOptions(logfile_name=logfile, console_logging_level=console_loglevel, logfile_logging_level=file_loglevel)
-    logger = utils.ColorLogger(name=__appname__, options=log_options)
-    # save_logger_options(log_options)
-    return logger
-
-def init(banner=True):
+def init():
     """ Clear Screen, display banner & start the logger. """
     CONSOLE.clear_screen()
-    if banner:
-        print(CONSOLE.get_app_banner(selection="random", banner_lst=banner_lst, appversion=__version__, creator="Designed by " + __author__))
+    if all_args["banner"]:
+        print(CONSOLE.get_app_banner(selection="random", banner_lst=BANNERS, appversion=__version__, creator="Designed by " + __author__))
     if all_args["debug"]:
         LOGLEVEL_CONSOLE = LOGLEVEL_SUCCESS
     else:
@@ -60,14 +53,16 @@ def init(banner=True):
         LOGLEVEL_FILE = LOGLEVEL_DISABLE
     global logger
     logger = get_logger(logger_name=__appname__, console_loglevel=LOGLEVEL_CONSOLE, file_loglevel=LOGLEVEL_FILE, logfile=all_args["logfile"])
-    logger.debug("Debug Mode Activated")
+    logger.info(f"Application Start")
+    logger.info(f"Logging levels : Console={LOGLEVEL_CONSOLE}; File={LOGLEVEL_FILE}; Logfile='{all_args['logfile']}'")
+    logger.debug("Confirm Debug Mode is Activated")
 
 def main(infile:Path = typer.Argument(..., exists=True, readable=True, resolve_path=True, show_default=False, help="The file name (with path) of the file to be analyzed."),
         outdir:Path = typer.Option(None, "--outdir", "-d", exists=False, resolve_path=True, show_default="Same directory as infile", help="Location of the output file", callback=callback_outdir),
         outfile:Path = typer.Option(None, "--outfile", "-o", exists=False, resolve_path=True, show_default="Same directory and filename (with new extension) as infile", help="File Name of the output file"),
-        banner:bool = typer.Option(True, help="Display a banner at start of the program", rich_help_panel="Customization and Utils"),
+        banner:bool = typer.Option(BANNER_DISPLAY, help="Display a banner at start of the program", rich_help_panel="Customization and Utils"),
         debug:bool = typer.Option(False, help="Enable debug mode on the console", rich_help_panel="Customization and Utils"),
-        logfile:Path = typer.Option(None, "--logfile", "-l", exists=False, resolve_path=True,  help="logfile of detailed activities (debug mode)", rich_help_panel="Customization and Utils"),
+        logfile:Path = typer.Option(str(LOG_FILE), "--logfile", "-l", exists=False, resolve_path=True,  help="logfile of detailed activities (debug mode)", rich_help_panel="Customization and Utils"),
         version:bool = typer.Option(False, "--version", "-v", callback=callback_version, is_eager=True, help="Display version of the program", rich_help_panel="Customization and Utils")
         ) -> None:
     # Put all arguments in a dictionnary & perform extra validation or default value assigment
@@ -80,7 +75,7 @@ def main(infile:Path = typer.Argument(..., exists=True, readable=True, resolve_p
     all_args["logfile"]=logfile
     all_args["version"]=version
     
-    init(banner=all_args["banner"])
+    init()
     validate_params()
 
     all_args_str=""
